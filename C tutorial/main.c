652d122fa59c0e53826083b1c7bf40b97508c73a
Ab1912 <122117358+Ab1912@users.noreply.github.com>
2023-01-26 01:16:07 +0530

    first commit

---
+++ C tutorial/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int a,b,c;
+    printf("Enter the values : \n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Value of A : %d\n",a);
+    printf("Value of B : %d\n",b);
+    printf("Total : %d",c);
+}
